1e3d4f2a78dc895ac64e2df49d52f37fdca6e95c
Kyle Crawford <kcrwfrd@gmail.com>
2022-02-21 23:46:16 -0500

    Update build settings for signing and notarization

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-PKGROOT=$(mktemp -d /tmp/dockutil-build-root-XXXXXXXXXXX)
-OUTPUTDIR=$(mktemp -d /tmp/dockutil-output-XXXXXXXXXXX)
-mkdir -p "${PKGROOT}/usr/local/bin/"
-cp ./scripts/dockutil "${PKGROOT}/usr/local/bin/"
-VERSION=$(./scripts/dockutil --version)
-echo packaging version: $VERSION
-OUTFILE="${OUTPUTDIR}/dockutil-${VERSION}.pkg"
-pkgbuild --root "$PKGROOT" --identifier dockutil.cli.tool --version $VERSION "$OUTFILE"
-echo $OUTFILE
-open $OUTPUTDIR
+xcodebuild -project dockutil.xcodeproj -scheme dockutil -configuration Release archive DEVELOPMENT_TEAM="Z5J8CJBUWC"
